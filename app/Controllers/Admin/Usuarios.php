ef4fbdf96fea263f427393a8db9d121f7d4c8112
Marcelo Joia <sitejoia@hotmail.com>
2022-08-26 17:28:00 -0300

    Criando o método procurar no controller e model usuários - parte 1

--- app/Controllers/Admin/Usuarios.php
+++ app/Controllers/Admin/Usuarios.php
@@ -5,21 +5,34 @@ namespace App\Controllers\Admin;
 use App\Controllers\BaseController;
 use App\Models\UsuarioModel;
 
-class Usuarios extends BaseController {
+class Usuarios extends BaseController
+{
 
     private $usuarioModel;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->usuarioModel = new UsuarioModel();
     }
 
-    public function index() {
-    
+    public function index()
+    {
+
         $data = [
             'titulo' => 'Listando os usuários',
             'usuarios' => $this->usuarioModel->findAll(),
         ];
         return view('Admin/Asuarios/index', $data);
+    }
 
+    public function procurar ()
+    {
+        if(!$this->request->isAJAX()){
+            exit('Página não encontrada.');
+        }
+        echo "<pre>";
+        print_r($this->request->getGet());
+        echo "</pre>";
+        exit;
     }
 }

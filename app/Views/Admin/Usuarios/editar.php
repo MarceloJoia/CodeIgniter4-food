2904743dea498d0c0c0957bcc72970202dd698b7
Marcelo Joia <sitejoia@hotmail.com>
2022-08-28 00:45:15 -0300

    Editando um usuário - parte 2

--- app/Views/Admin/Usuarios/editar.php
+++ app/Views/Admin/Usuarios/editar.php
@@ -6,6 +6,7 @@
 <!-- styles -->
 <?= $this->section('estilos') ?>
 
+
 <?= $this->endSection() ?>
 
 <!-- contents -->
@@ -25,29 +26,21 @@
 
                 <form class="forms-sample">
 
-                    <?=$this->include('Admin/Usuarios/form');?>
-                   
+                    <?= $this->include('Admin/Usuarios/form'); ?>
+
+                    <a href="<?= site_url("admin/usuarios/show/$usuario->id"); ?>" class="btn btn-success btn-sm mr-3 mdi mdi-reply">
+                        Voltar
+                    </a>
+                    <button type="submit" class="btn btn-primary btn-sm mr-3 mdi mdi-content-save">
+                        Salvar
+                    </button>
+
                 </form>
 
             </div>
 
             <!-- Footer -->
             <div class="card-footer bg-light">
-
-                <a href="<?= site_url("admin/usuarios/editar/$usuario->id"); ?>" class="btn btn-success btn-sm mr-3 mdi mdi-reply">
-                    Voltar
-                </a>
-
-                <a href="<?= site_url("admin/usuarios/editar/$usuario->id"); ?>" class="btn btn-primary btn-sm mr-3 mdi mdi-pencil">
-                    Editar
-                </a>
-
-                <a href="<?= site_url("admin/usuarios/editar/$usuario->id"); ?>" class="btn btn-danger btn-sm mr-3 mdi mdi-delete-forever">
-                    Excluir
-                </a>
-
-                <hr>
-
                 <a href="https://api.whatsapp.com/send?phone=5516991502332&text=Estou%20precisando%20de%20ajuda,%20com%20o%20LetsFood!" target="_blank" alt="Joia Marketing" title="Joia Marketing" class="btn btn-link btn-sm mdi mdi-bullhorn">
                     Precisa de ajuda? Fale com a gente!
                 </a>
@@ -59,7 +52,11 @@
 
 <?= $this->endSection() ?>
 
+
 <!-- scripts -->
 <?= $this->section('scripts') ?>
+
+<script src="<?= site_url('Admin/vendors/mask/app.js') ?>"></script>
+<script src="<?= site_url('Admin/vendors/mask/jquery.mask.min.js') ?>"></script>
 
 <?= $this->endSection() ?>

5efdc78683eb3124403e589b847ef2762e4b61e4
Marcelo Joia <sitejoia@hotmail.com>
2022-08-28 01:25:58 -0300

    Editando um usuário parte - 3

--- app/Views/Admin/Usuarios/form.php
+++ app/Views/Admin/Usuarios/form.php
@@ -2,22 +2,53 @@
 
     <div class="form-group col-md-6">
         <label for="name">Nome: </label>
-        <input type="text" class="form-control" name="name" id="name" value="<?= $usuario->name; ?>">
+        <input type="text" class="form-control" name="name" id="name" value="<?= esc($usuario->name); ?>">
     </div>
 
     <div class="form-group col-md-6">
         <label for="cpf">CPF: </label>
-        <input type="text" class="form-control cpf" name="cpf" id="cpf" value="<?= $usuario->cpf; ?>">
+        <input type="text" class="form-control cpf" name="cpf" id="cpf" value="<?= esc($usuario->cpf); ?>">
     </div>
 
     <div class="form-group col-md-6">
         <label for="celular">Celular: </label>
-        <input type="text" class="form-control phone_with_ddd" name="celular" id="celular" value="<?= $usuario->telefone; ?>">
+        <input type="text" class="form-control phone_with_ddd" name="celular" id="celular" value="<?= esc($usuario->telefone); ?>">
     </div>
 
     <div class="form-group col-md-6">
         <label for="email">E-mail: </label>
-        <input type="text" class="form-control" name="email" id="email" value="<?= $usuario->email; ?>">
+        <input type="text" class="form-control" name="email" id="email" value="<?= esc($usuario->email); ?>">
+    </div>
+
+    
+    <div class="col-md-12">
+        <hr class="pb-3 pt-0">
+        <h3>Acesso do usuário</h3>
+    </div>
+    <div class="form-group col-md-6">
+        <label for="ativo">Ativo: </label>
+        <select name="ativo" class="form-control ">
+            <?php if ($usuario->id) : ?>
+                <option value="1" <?= $usuario->ativo ? 'selected' : ''; ?>>Sim</option>
+                <option value="0" <?= !$usuario->ativo ? 'selected' : ''; ?>>Não</option>
+            <?php else : ?>
+                <option value="1">Sim</option>
+                <option value="0" selected="">Não</option>
+            <?php endif; ?>
+        </select>
+    </div>
+
+    <div class="form-group col-md-6">
+    <label for="ativo">Perfil: </label>
+        <select name="is_admin" class="form-control ">
+            <?php if ($usuario->id) : ?>
+                <option value="1" <?= $usuario->is_admin ? 'selected' : ''; ?>>Administrador</option>
+                <option value="0" <?= !$usuario->is_admin ? 'selected' : ''; ?>>Usuário</option>
+            <?php else : ?>
+                <option value="1">Sim</option>
+                <option value="0" selected="">Não</option>
+            <?php endif; ?>
+        </select>
     </div>
 
 </div>
@@ -29,7 +60,7 @@
     </div>
 
     <div class="form-group col-md-6">
-        <label for="confirmatio_password">Confirm Password</label>
-        <input type="password" class="form-control" name="confirmatio_password" id="confirmatio_password">
+        <label for="password_confirmatio">Confirm Password</label>
+        <input type="password" class="form-control" name="password_confirmatio" id="password_confirmatio">
     </div>
 </div>
